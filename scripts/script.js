800ade9deaf7471a17b99e6bc14b6856f65a9d0e
PesukarhuTG <ganzhinovat@yandex.ru>
2023-08-24 00:01:04 +0300

    feat: implement getting data city data from API

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,37 @@
-/* launch library for select*/
-const citySelect = document.querySelector('#city');
-const cityChoices = new Choices(citySelect, {
-  searchEnabled: false,
-  itemSelectText: '',
-});
+const API_URL = 'https://workspace-methed.vercel.app/';
+const LOCATION_URL = 'api/locations';
+
+/* API requests */
+const getData = async (url, cbSuccess, cbError) => {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    cbSuccess(data);
+  } catch (err) {
+    cbError(err);
+  }
+};
+
+const init = () => {
+  /* launch library for select*/
+  const citySelect = document.querySelector('#city');
+  const cityChoices = new Choices(citySelect, {
+    searchEnabled: true,
+    itemSelectText: '',
+  });
+
+  getData(
+    `${API_URL}${LOCATION_URL}`,
+    (locationData) => {
+      const locations = locationData.map(location => ({
+        value: location,
+      }));
+      cityChoices.setChoices(locations, 'value', 'label', true);
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
+};
+
+init();
